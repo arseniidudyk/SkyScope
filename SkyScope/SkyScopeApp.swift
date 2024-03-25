997aad9ec342ceb9f8e2808b7951a61958f27bc6
Arsenii Dudyk <arsengggj@gmail.com>
2024-03-25 15:38:31 +0100

    Initial Commit

---
+++ SkyScope/SkyScopeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkyScopeApp.swift
+//  SkyScope
+//
+//  Created by Arsenii Dudyk on 25/03/24.
+//
+
+import SwiftUI
+
+@main
+struct SkyScopeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
